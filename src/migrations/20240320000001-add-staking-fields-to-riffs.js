c78c9593c2452cb505e27300c885c4e4331f85e4
aiblockbuilder <continuebuilding@gmail.com>
2025-07-05 02:41:56 -0700

    update: migration

--- src/migrations/20240320000001-add-staking-fields-to-riffs.js
+++ src/migrations/20240320000001-add-staking-fields-to-riffs.js
@@ -2,28 +2,45 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.addColumn('riffs', 'minimumStakeAmount', {
-      type: Sequelize.INTEGER,
-      allowNull: true,
-      defaultValue: 100
-    });
+    // Check if minimumStakeAmount column exists
+    const tableDescription = await queryInterface.describeTable('riffs');
+    
+    if (!tableDescription.minimumStakeAmount) {
+      await queryInterface.addColumn('riffs', 'minimumStakeAmount', {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+        defaultValue: 100
+      });
+    }
 
-    await queryInterface.addColumn('riffs', 'lockPeriodDays', {
-      type: Sequelize.INTEGER,
-      allowNull: true,
-      defaultValue: 30
-    });
+    if (!tableDescription.lockPeriodDays) {
+      await queryInterface.addColumn('riffs', 'lockPeriodDays', {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+        defaultValue: 30
+      });
+    }
 
-    await queryInterface.addColumn('riffs', 'useProfileDefaults', {
-      type: Sequelize.BOOLEAN,
-      allowNull: false,
-      defaultValue: true
-    });
+    if (!tableDescription.useProfileDefaults) {
+      await queryInterface.addColumn('riffs', 'useProfileDefaults', {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+        defaultValue: true
+      });
+    }
   },
 
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.removeColumn('riffs', 'minimumStakeAmount');
-    await queryInterface.removeColumn('riffs', 'lockPeriodDays');
-    await queryInterface.removeColumn('riffs', 'useProfileDefaults');
+    const tableDescription = await queryInterface.describeTable('riffs');
+    
+    if (tableDescription.minimumStakeAmount) {
+      await queryInterface.removeColumn('riffs', 'minimumStakeAmount');
+    }
+    if (tableDescription.lockPeriodDays) {
+      await queryInterface.removeColumn('riffs', 'lockPeriodDays');
+    }
+    if (tableDescription.useProfileDefaults) {
+      await queryInterface.removeColumn('riffs', 'useProfileDefaults');
+    }
   }
 }; 
--- src/migrations/20240320000002-add-metadata-url-to-riffs.js
+++ src/migrations/20240320000002-add-metadata-url-to-riffs.js
@@ -2,14 +2,22 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.addColumn('riffs', 'metadataUrl', {
-      type: Sequelize.STRING,
-      allowNull: true,
-      comment: 'IPFS URI for NFT metadata (ipfs://{cid})'
-    });
+    const tableDescription = await queryInterface.describeTable('riffs');
+    
+    if (!tableDescription.metadataUrl) {
+      await queryInterface.addColumn('riffs', 'metadataUrl', {
+        type: Sequelize.STRING,
+        allowNull: true,
+        comment: 'IPFS URI for NFT metadata (ipfs://{cid})'
+      });
+    }
   },
 
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.removeColumn('riffs', 'metadataUrl');
+    const tableDescription = await queryInterface.describeTable('riffs');
+    
+    if (tableDescription.metadataUrl) {
+      await queryInterface.removeColumn('riffs', 'metadataUrl');
+    }
   }
 }; 
